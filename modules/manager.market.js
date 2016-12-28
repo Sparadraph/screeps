c95ec3f14606537566e1e41a6cbe9eab1233e2a8
nfl <nfl@odoo.com>
2016-12-28 21:16:39 +0100

    [ADD] Market get products

--- modules/manager.market.js
+++ modules/manager.market.js
@@ -2,15 +2,16 @@ var managerMarket = {
     init: function() {
         Memory.market = {
             time: Game.time,
-            offers: {},
+            b_offers: {},
+            s_offers: {},
         }
     },
 
     initRoom: function(room) {
         room.memory.market = {
             energy: {
-                min: 10000,
-                max: 80000,
+                min: 0,
+                max: 100000,
                 val: 0.03,
             },
         };
@@ -30,22 +31,45 @@ var managerMarket = {
                 return s.structureType == 'terminal';
             }
         })
+
+        var energy_pu = 0.03;
         for(var r in room.memory.market) {
             var data = room.memory.market[r];
             if(terminals.length > 0 && terminals[0].store[r] > data.max){
-                if(!Memory.market.offers[r]) {
-                    Memory.market.offers[r] = Game.market.getAllOrders({
+                if(!Memory.market.b_offers[r]) {
+                    Memory.market.b_offers[r] = Game.market.getAllOrders({
                         'type': ORDER_BUY,
                         'resourceType': r,
                     });
                 }
 
-                Memory.market.offers[r].forEach(function(offer) {
-                    var amount = Math.min(offer.remainingAmount, terminals[0].store[r] - data.max, 20000);
+                Memory.market.b_offers[r].forEach(function(offer) {
+                    var amount = Math.min(offer.remainingAmount, terminals[0].store[r] - data.max, 2000);
                     var energy_cost = Game.market.calcTransactionCost(amount, offer.roomName, room.name);
-                    var energy_pu = 0.03;
                     var lose = amount * data.val + energy_pu * energy_cost;
                     var win = offer.price * amount;
+                    if(lose < win) {
+                        console.log("Traiding (" + r + ") - " + amount + " (" + offer.price + ")");
+                        console.log("Win: " + win + " - Lose: " + lose);
+                        Game.market.deal(offer.id, amount, room.name);
+                    }
+                })
+            }
+
+
+            if(terminals.length > 0 && terminals[0].store[r] < data.min) {
+                if(!Memory.market.s_offers[r]) {
+                    Memory.market.s_offers[r] = Game.market.getAllOrders({
+                        'type': ORDER_SELL,
+                        'resourceType': r,
+                    });
+                }
+
+                Memory.market.s_offers[r].forEach(function(offer) {
+                    var amount = Math.min(offer.remainingAmount, data.min - terminals[0].store[r], 1000);
+                    var energy_cost = Game.market.calcTransactionCost(amount, offer.roomName, room.name);
+                    var lose = offer.price * amount + energy_pu * energy_cost;
+                    var win = amount * data.val;
                     if(lose < win) {
                         console.log("Traiding (" + r + ") - " + amount + " (" + offer.price + ")");
                         console.log("Win: " + win + " - Lose: " + lose);
